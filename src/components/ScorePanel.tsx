7252cdf5798205f3c0af799fabe62f3101fc9c97
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-06 00:28:30 +0100

    invited

--- src/components/ScorePanel.tsx
+++ src/components/ScorePanel.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface IProps {
 	playerResult: {
 		name: string;
@@ -6,11 +8,13 @@ interface IProps {
 }
 
 export const ScorePanel = ({ playerResult }: IProps) => {
+	const [paid, setPaid] = useState(false);
 	return (
-		<div className="bg-purple-400 mr-2 w-24 px-1 py-3 flex justify-center rounded  border-blue-950 border shadow shadow-blue-900">
+		<div className="bg-purple-400 mr-2 w-24 px-1 py-3 justify-center rounded flex  border-blue-950 border shadow shadow-blue-900">
 			<div>
-				<p className="font-bold">{playerResult.name}</p>
-				<p>{playerResult.score}</p>
+				<p className="font-bold flex justify-center">{playerResult.name}{paid && (<span>*</span>)}</p>
+				<p className="flex justify-center">{playerResult.score}</p>
+				<button onClick={() => setPaid(!paid)}>invited</button>
 			</div>
 		</div>
 	);
