86a9eaaf44890e5e1641e73e70cd4a4645758339
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-05 23:00:42 +0100

    highlight

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 			<Header/>
 			<p>Welcome to this site.</p>
 
-			<InfoBox title="How to log in" body="LVjorem ipsum dolor sit amet consectetur adipisicing elit. Vel delectus reprehenderit odio ratione totam quod, explicabo eaque cumque recusandae aut repellat quisquam a illo maxime dicta modi. Laboriosam, iusto quis!" stars={3} />
+			<InfoBox title="How to log in" body="LVjorem ipsum dolor sit amet consectetur adipisicing elit. Vel delectus reprehenderit odio ratione totam quod, explicabo eaque cumque recusandae aut repellat quisquam a illo maxime dicta modi. Laboriosam, iusto quis!" stars={3} highlight={true} />
 			<InfoBox title="How to sign out" body="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum excepturi, voluptatibus voluptatem autem beatae quae magnam possimus repellendus saepe! Autem illo atque optio eum inventore maiores magni accusantium error? Sed!" stars={5} />
 
 			<Footer/>
--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -4,11 +4,13 @@ interface IProps {
 	title: string;
 	body: string;
 	stars: number;
+	highlight?: boolean;
 }
 
-export const InfoBox = ({ title, body, stars} : IProps) => {
+export const InfoBox = ({ title, body, stars, highlight = false} : IProps) => {
+	const sectionStyle = highlight ? "bg-yellow-300 border-4 border-red-500 p-4 rounded mt-4 shadow-gray-500 shadow-lg" : "bg-orange-300 p-4 rounded mt-4 shadow-gray-500 shadow-lg";
 	return (
-		<section className="bg-orange-300 p-4 rounded mt-4 shadow-gray-500 shadow-lg">
+		<section className={sectionStyle}>
 			<h2 className="text-2xl mb-2 flex gap-2"><span>{title}</span> <div>{"*".repeat(stars)}</div></h2>
 			<p>{body}</p>
 		</section>
